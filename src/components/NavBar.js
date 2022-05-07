65cedce66f76f9392abd577c29daf64e68e1d86d
pedrokpdo <pedromotoehm@gmail.com>
2022-05-07 10:04:33 -0300

    navBAR

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,11 @@
+import navIcon from '../pokeapi_256.png'
+
 export const NavBar = () => {
     return (
         <nav>
-            <div>PokeApi</div>
+            <div>
+                <img alt='NavIcon' src={navIcon} className='navbar-img'></img>
+            </div>
         </nav>
     )
 }
